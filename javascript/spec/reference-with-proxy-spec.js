33cd9b8d250ac3a39b641a62741736643bd29159
kaspernj <kasper@diestoeckels.de>
2025-09-23 15:45:33 +0000

    Added spec for readAttribute

--- javascript/spec/reference-with-proxy-spec.js
+++ javascript/spec/reference-with-proxy-spec.js
@@ -36,5 +36,28 @@ describe("referenceWithProxy", () => {
     const result = await stringObject.__serialize()
 
     expect(result).toEqual(["test1", "test2"])
+
+    const firstValue = await stringObject.readAttribute(0)
+    const secondValue = await stringObject.readAttribute(1)
+
+    expect(firstValue).toEqual("test1")
+    expect(secondValue).toEqual("test2")
+  })
+
+  it("reads attributes from a reference", async () => {
+    const testArray = await shared.client.newObjectWithReference("Array")
+
+    await testArray.callMethod("push", "test1")
+    await testArray.callMethod("push", "test2")
+
+    const result = await testArray.serialize()
+
+    expect(result).toEqual(["test1", "test2"])
+
+    const firstValue = await testArray.readAttribute(0)
+    const secondValue = await testArray.readAttribute(1)
+
+    expect(firstValue).toEqual("test1")
+    expect(secondValue).toEqual("test2")
   })
 })
